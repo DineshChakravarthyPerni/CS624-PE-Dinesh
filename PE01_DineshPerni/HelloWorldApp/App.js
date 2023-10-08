57977f4246d4e86be2671dde69647efbab87fc8f
DineshChakravarthyPerni <122706101+DineshChakravarthyPerni@users.noreply.github.com>
2023-10-08 06:01:18 +0000

    PE01_CS624Submission_DineshPerni

---
+++ PE01_DineshPerni/HelloWorldApp/App.js
@@ -0,0 +1,52 @@
+/*
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
+// App.js
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Name: Dinesh Chakravarthy Perni</Text>
+      <Text style={styles.text}>Degree Program: MSCS</Text>
+      <Text style={styles.text}>School: STC</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default App;
